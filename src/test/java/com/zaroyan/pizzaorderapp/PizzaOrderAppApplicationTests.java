c3431eac8688d24198d61abf6c2480a92646d072
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-04-13 22:28:51 +0300

    "Initial commit"

---
+++ src/test/java/com/zaroyan/pizzaorderapp/PizzaOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaroyan.pizzaorderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
